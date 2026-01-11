f74511a5bbbd9b495e8dc320a0b982de0d43ddc3
Lee Hansel Solevilla <sshanzel@yahoo.com>
2026-01-11 17:07:14 +0400

    refactor: users to user table

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,7 +1,9 @@
+import {relations} from 'drizzle-orm';
 import {pgTable, uuid, text, timestamp, unique} from 'drizzle-orm/pg-core';
+import {document} from './document.ts';
 
 export const users = pgTable(
-  'users',
+  'user',
   {
     id: uuid().defaultRandom().primaryKey(),
     email: text().notNull(),
@@ -11,3 +13,7 @@ export const users = pgTable(
   },
   t => [unique('users_email_unique').on(t.email)]
 );
+
+export const usersRelations = relations(users, ({many}) => ({
+  documents: many(document),
+}));
