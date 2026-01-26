524c28db68b4d60bc31d99392aa91fcc93c0912f
Lee Hansel Solevilla <sshanzel@yahoo.com>
2026-01-26 14:57:23 +0400

    fix: unnecessary change

--- infra/src/iam/index.ts
+++ infra/src/iam/index.ts
@@ -36,12 +36,12 @@ export const pubsubInvokerServiceAccount = new gcp.serviceaccount.Account(
   {dependsOn: enabledApis}
 );
 
-// API permissions: Pub/Sub editor, Storage admin
+// API permissions: Pub/Sub publisher, Storage admin
 new gcp.projects.IAMMember(
-  'api-pubsub-editor',
+  'api-pubsub-publisher',
   {
     project: projectId,
-    role: 'roles/pubsub.editor',
+    role: 'roles/pubsub.publisher',
     member: pulumi.interpolate`serviceAccount:${apiServiceAccount.email}`,
   },
   {dependsOn: enabledApis}
